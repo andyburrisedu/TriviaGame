7318282d14f8989ca1d89da5986b071c1b97095f
Enzo Elacqua <20009232@CTECS-463-9.local>
2020-02-12 13:49:51 -0500

    added a constructor

--- src/com/csa/trivia/TriviaQuestion.java
+++ src/com/csa/trivia/TriviaQuestion.java
@@ -1,5 +1,32 @@
-//returns an arraylist of TriviaQuestion 
-  public class TriviaQuestion.java {
+//assume other class returns an arraylist of TriviaQuestion 
+//git add file name
+//got commit "whatever happened"
+//git push origin master
+// to get what andy has done git pull origin master
+// to download again git clone and the url on cloner download in the page
 
+
+//make a string for questions and each of the answers, then make an int from 0 to 3 
+public class TriviaQuestion {
+  
+  String questions;
+  String a;
+  String b;
+  String c;
+  String d;
+  int answer;
+  
+  public TriviaQuestion(String questions, String a, String b, String c, String d, int answer){
+    this.questions = questions;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    this.answer = answer;
   }
+  
+  public static void Answer (int answer){
     
+  }
+  
+}
